ad07d04f0836c556cc292e41cd73c9987fe4c870
Jeremy Dryden <jeremy.dryden@gmail.com>
2019-10-08 11:45:25 -0400

    added the reducer and dispatcher

--- redux-simple.js
+++ redux-simple.js
@@ -1,4 +1,27 @@
 import { createStore } from "redux";
 
-const hello = () => "hello";
-const store = createStore(hello);
+const defaultState = {
+  welcome: "Hi",
+  otherStuff: "some stuff"
+};
+
+const greeting = (state = defaultState, action) => {
+  switch (action.type) {
+    case "GREET_ME":
+      return { ...state, welcome: "Hello Scott" };
+    case "GREET_WORLD":
+      return { ...state, welcome: "Hello World" };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(greeting);
+console.log(store.getState());
+
+store.dispatch({
+  type: "GREET_ME"
+});
+
+// console.log(store);
+console.log(store.getState());
